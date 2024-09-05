e6c587e1844a3502c171297308244b72972cc84a
Chinapat0843 <chinapat.onp@gmail.com>
2024-09-05 09:40:41 +0700

    edit bash script for run container

--- run_docker.sh
+++ run_docker.sh
@@ -6,5 +6,5 @@ export $(grep -v '^#' .env | xargs)
 # Define the Docker command using the .env file
 docker run -v C:/chinapat.onp/source/R_research_package/miniCRAN:/miniCRAN \
     -e R_VERSION_DATE="$R_VERSION_DATE" \
-    --env-file="$ENV_FILE" \
+    --env-file=$ENV_FILE \
     minicran
